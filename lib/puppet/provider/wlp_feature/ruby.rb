3db2323bc53199d5715d63c16998570d01c8b549
ipcrm <mcadorette@puppetlabs.com>
2016-06-28 22:36:20 -0400

    [WIP] wlp_feature updated to support resource

--- lib/puppet/provider/wlp_feature/ruby.rb
+++ lib/puppet/provider/wlp_feature/ruby.rb
@@ -1,6 +1,11 @@
 Puppet::Type.type(:wlp_feature).provide(:ruby) do
 
-  def get_base_path
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  def self.get_base_path
     base_path = '/usr/local/wlp'
     if File.directory?(base_path)
       return base_path
@@ -9,51 +14,85 @@ Puppet::Type.type(:wlp_feature).provide(:ruby) do
     end
   end
 
-  def get_installed_features
+  def self.get_installed_features
     base_path = get_base_path
     begin
       feature_command = "#{base_path}/bin/productInfo"
       command = [feature_command, 'featureInfo']
-      installed_features = Puppet::Util::Execution.execute(command, :uid => resource[:wlp_user], :combine => true, :failonfail => true)
+      installed_features = Puppet::Util::Execution.execute(command, :combine => true, :failonfail => true)
     rescue Puppet::ExecutionFailure => e
       Puppet.debug("get_installed_features failed to generate report -> #{e.inspect}")
       return nil
     end
 
     return nil if installed_features.empty?
-    installed_features
+
+    features = Array.new
+    installed_features.split(/\n/).each do |f|
+      features.push(f.split(' ')[0])
+    end
+
+    features
+  end
+
+  def self.instances
+    features = get_installed_features
+    instances = []
+      features.collect do |f|
+        f_new            = {}
+        f_new[:ensure]   = :present
+        f_new[:provider] = :ruby
+        f_new[:name]     = f
+        instances << new(f_new)
+      end
+
+    instances
+  end
+
+  def self.prefetch(resources)
+    instances.each do |prov|
+      if resource = resources[prov.name]
+        resource.provider = prov
+      end
+    end
   end
 
   def exists?
-    get_installed_features.include?(resource[:name])
+    @property_hash[:ensure] == :present || false
   end
 
   def create
+    @property_flush[:ensure] = :present
+  end
+
+  def destroy
+    @property_flush[:ensure] = :absent
+  end
+
+  def configure_feature
     base_path = self.class.get_base_path
     installutility = "#{base_path}/bin/installUtility"
-
     arguments = Array.new
-    arguments.push(resource[:name])
-    arguments.push('--acceptLicense')                   if resource[:accept_license] == :true
-    arguments.push('--to=extension')                    if resource[:install_type] == 'extension'
-    arguments.push("--from=#{resource[:install_from]}") if resource[:install_from]
-    arguments.push("--downloadDependencies=false")      if resource[:download_deps] != :true
-    arguments.push("--verbose")                         if resource[:verbose] == :true
+    if @property_flush[:ensure] == :absent
+      arguments = ['uninstall','--verbose','--noPrompts',"#{resource[:name]}"]
+    else
+      arguments.push('install')
+      arguments.push(resource[:name])
+      arguments.push('--acceptLicense')                   if resource[:accept_license] == :true
+      arguments.push('--to=extension')                    if resource[:install_type] == 'extension'
+      arguments.push("--from=#{resource[:install_from]}") if resource[:install_from]
+      arguments.push("--downloadDependencies=false")      if resource[:download_deps] != :true
+      arguments.push("--verbose")                         if resource[:verbose] == :true
+    end
 
-    command = arguments.unshift('install')
     command = arguments.unshift(installutility).flatten
     Puppet::Util::Execution.execute(command, :uid => resource[:wlp_user], :combine => true, :failonfail => true)
   end
 
-  def destroy
-    base_path = self.class.get_base_path
-    installutility = "#{base_path}/bin/installUtility"
-
-    arguments = ['uninstall','--verbose','--noPrompts',"#{resource[:name]}"]
-    command = arguments.unshift(installutility).flatten.uniq
-
-    Puppet::Util::Execution.execute(command, :uid => resource[:wlp_user], :combine => true, :failonfail => true)
+  def flush
+    configure_feature
+    if self.class.get_installed_features.include?(resource[:name])
+      @property_hash = { :ensure => :present, :provider => :ruby, :name => resource[:name] }
+    end
   end
-
-
 end
